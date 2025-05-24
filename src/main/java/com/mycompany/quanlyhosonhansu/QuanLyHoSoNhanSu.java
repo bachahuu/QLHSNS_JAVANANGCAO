e9ade9da2c7758f615281f6430fa8e78cb673ac8
bachahuu <bachahuu182004@gmail.com>
2025-05-24 21:38:14 +0700

    Initial commit - upload local project to GitHub

---
+++ src/main/java/com/mycompany/quanlyhosonhansu/QuanLyHoSoNhanSu.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.quanlyhosonhansu;
+
+/**
+ *
+ * @author Windows
+ */
+public class QuanLyHoSoNhanSu {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/model/Connect.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Windows
+ */
+public class Connect {
+      private String host ;
+      private String user ;
+      private String pass ;
+      private String url ;
+      private Connection conn;
+    public Connect(){
+         host = "localhost";
+         user = "root";
+         pass ="";
+         url ="jdbc:mysql://localhost:3306/qlhsns";
+    }
+    public Connection getConnection() throws SQLException{
+          try {
+           if(conn != null){
+             return conn; 
+           } 
+            conn = DriverManager.getConnection(url, user, pass);
+          } catch (Exception e) {
+              e.printStackTrace();
+          }
+          return conn;
+         
+      }
+}
